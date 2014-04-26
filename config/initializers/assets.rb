3c3632cd25ff48c8fb647fb45547bdd95d1c381d
Nikki Shah <Nikki@Nikkis-MacBook-Pro.local>
2014-04-26 17:19:27 -0400

    changed signup to bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( login.css )
 Rails.application.config.assets.precompile += %w( login.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
