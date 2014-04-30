f30b864fe4ea74b0d33c3c80ad257aef2029163f
Anuhya Ghorakavi <anuhya.ghorakavi@gmail.com>
2014-04-30 18:58:44 -0400

    added a bunch of files working on putting all the html and styling like we had before.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,16 @@
+# add all the css files from the original application
 Rails.application.config.assets.precompile += %w( login.css )
-Rails.application.config.assets.precompile += %w( login.js )
+Rails.application.config.assets.precompile += %w( profile.css )
+Rails.application.config.assets.precompile += %w( navbar.css )
+Rails.application.config.assets.precompile += %w( maps.css )
+Rails.application.config.assets.precompile += %w( groups.css )
+Rails.application.config.assets.precompile += %w( group_view.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+
+# add all the javascript files from the original application
+Rails.application.config.assets.precompile += %w( login.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery-1.9.0.min.js )
+
+
