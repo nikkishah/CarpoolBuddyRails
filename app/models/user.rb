7479572e5583627d8ec7cb1c9e92d320dbba7798
Nikki Shah <Nikki@Nikkis-MacBook-Pro.local>
2014-05-05 18:04:09 -0400

    changed sessions kind of

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,16 @@ class User < ActiveRecord::Base
 	has_one :calendar
 
 	def User.new_remember_token
-		SecureRandom.urlsafe_base64
-	end
+    SecureRandom.urlsafe_base64
+  end
 
-	def User.digest(token)
-		Digest::SHA1.hexdigest(token.to_s)
-	end
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
-	private
+  private
 
-	def create_remember_token
-		self.remember_token = User.digest(User.new_remember_token)
-	end
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
